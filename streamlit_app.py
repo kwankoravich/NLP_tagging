355af9bf13d6bbac8cb9af4268a8ec0e54487040
kwankoravich <41057176+kwankoravich@users.noreply.github.com>
2022-03-20 19:44:11 +0000

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,57 @@ st.sidebar.header('Choose Prediction model')
 st.sidebar.subheader('File Upload')
 # uploaded_file = st.file_uploader(label = 'Upload your file, in .txt format', type = ['txt'])
 
-model_choice = st.sidebar.selectbox('Select Prediction Model', ['Sentence Segmentation', 'Name Entity Recignition'], key = '1')
+model_choice = st.sidebar.selectbox('Select Prediction Model', ['Sentence Segmentation', 'Name Entity Recognition'], key = '1')
 
 if model_choice == 'Sentence Segmentation':
         uploaded_file = st.file_uploader(label = 'Upload your file, in .txt format', type = ['txt'])
         if uploaded_file is not None:
                 file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
                 st.write(file_details)
+                st.success('Text file has been already uploaded, ready for predicion')
+                st.info("Press Predict button to show the tagging")
+        submit = st.button('Predict')
+        if submit:
+                st.ballons()
+                ct = CRFTagger()
+                # ct = sklearn_crfsuite.CRF()
+                ct.set_model_file('model/crf_sentence.tagger')
+                
+                #Prediction
+                preds_list = []
+                test_data = uploaded_file
+                for sent in test_data:
+                        sent_preds = [x[1] for x in ct.tag([s[0] for s in sent])]
+                        preds_list.extend(sent_preds)
+                
+                raw_test_data = [x for x in [s[0] for s in sent]]
+                preds_list = list(zip(raw_test_data, preds_list))
+                st.text(preds_list[:10])
+
+
+if model_choice == 'Name Entity Recognition':
+        uploaded_file = st.file_uploader(label = 'Upload your file, in .txt format', type = ['txt'])
+        if uploaded_file is not None:
+                file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
+                st.write(file_details)
+                st.success('Text file has been already uploaded, ready for predicion')
+                st.info("Press Predict button to show the tagging")
+        submit = st.button('Predict')
+        if submit:
+                st.ballons()
+                ct = CRFTagger()
+                # ct = sklearn_crfsuite.CRF()
+                ct.set_model_file('model/crf_ner.tagger')
+                
+                #Prediction
+                preds_list = []
+                test_data = uploaded_file
+                for sent in test_data:
+                        sent_preds = [x[1] for x in ct.tag([s[0] for s in sent])]
+                        preds_list.extend(sent_preds)
+                
+                raw_test_data = [x for x in [s[0] for s in sent]]
+                preds_list = list(zip(raw_test_data, preds_list))
+                st.text(preds_list[:10])
 
 
