0f37125fad2b9d2ea04a5677cf7d0816c88cbac9
kwankoravich <41057176+kwankoravich@users.noreply.github.com>
2022-03-24 19:37:55 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,8 @@ if model_choice == 'Sentence Segmentation':
                         # st.text(sent)
                         # sent_preds = [x[1] for x in ct.tag([s[0] for s in sent])]
                         # preds_list.extend(sent_preds)
-                st.text([x[1] for x in ct.tag([test_data])])
+                preds_list = [x[1] for x in ct.tag([test_data])]
+                st.text(preds_list)
                 # st.text(preds_list[:10])
                 # raw_test_data = [x for x in [s[0] for s in sent]]
                         # raw_data = [x for x in [s[0] for s in sent]]
