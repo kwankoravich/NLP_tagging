49d63a0ce46bed410fd9ef716afd903b6db6ec85
kwankoravich <41057176+kwankoravich@users.noreply.github.com>
2022-03-24 19:50:32 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,12 +54,12 @@ if model_choice == 'Sentence Segmentation':
                 stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
                 test_data = stringio.read()
                 # st.text(test_data[:10])
-                # for sent in test_data:
+                for sent in test_data:
                 # for sent in stringio.read():
                         # st.text(sent)
                         # sent_preds = [x[1] for x in ct.tag([s[0] for s in sent])]
                         # preds_list.extend(sent_preds)
-                preds_list = [x[1] for x in ct.tag([test_data])]
+                        preds_list = [x[1] for x in ct.tag(sent)]
                 st.text(len(preds_list))
                 # st.text(['fddgfdgd','dgdgdgdgdg','dgdgdgdgdg'])
                 # st.text(preds_list[:10])
